2f2b832eb59cfb31e9d6dce4116d9fd8752700b2
Taniya <taniyapal369007@gmail.com>
2026-03-09 16:46:22 +0530

    Updated README and fixed API URL

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 const app = require("./src/app");
 const connectDB = require('./src/config/db');
+const PORT = process.env.PORT || 3000;
 
 connectDB(); 
 
-app.listen(3000,()=>{
-    console.log ("server is running");
+app.listen(PORT,()=>{
+    console.log (`server is running on port ${PORT}`);
 
     
     
